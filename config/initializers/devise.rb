031cc052783de21a1f24e8b56fbd768da340c7d4
kawashima-nagisa <nagisa@hab-co.jp>
2024-11-11 10:12:04 +0900

    メール送信者の設定を環境変数から取得するように変更し、ホームページのデザインを改善。新しいスタイルシートを追加し、ナビゲーションバーのリンクにテキストカラーを適用。検索フォームのラベルにスタイルを追加し、Aboutページのスタイルを強化。

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -24,7 +24,8 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = "example@example.com"
+  config.mailer_sender = Rails.application.credentials.dig(:mailer, :sender_email)
+
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
